5d657ca7315e2395745258e3e47cb1ba814fe4f3
datagod <william.mcevoy@gmail.com>
2021-03-27 20:05:49 -0400

    Add files via upload

---
+++ go.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#sudo python3 runtext.py --led-cols 64 -r32 -b50 --led-slowdown-gpio=2 -t "We are gone on a walk"
+#sudo ./clock -f ../fonts/7x13.bdf --led-chain=1 -d "%A" -d "%H:%M:%S" --led-slowdown-gpio=2 -b 50 -C 0,200,0
+cd ArcadeRetroClock
+sudo python3 ArcadeRetroClock.py
